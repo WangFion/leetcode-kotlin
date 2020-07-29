42b2bd4c0ddc2b71b6500f3d24d7b90503966789
wf-jiuzhou <wangfeisdc@jiuzhoutech.com>
2020-07-29 09:22:54 +0800

    daily 18、二叉树的最大深度

--- app/src/main/java/com/wf/leetcode/daily/entity/TreeNode.kt
+++ app/src/main/java/com/wf/leetcode/daily/entity/TreeNode.kt
@@ -1,5 +1,7 @@
 package com.wf.leetcode.daily.entity
 
+import java.util.*
+
 /**
  * leetcode -> com.wf.leetcode.daily.entity -> TreeNode
  *
@@ -9,4 +11,57 @@ package com.wf.leetcode.daily.entity
 class TreeNode(var value: Int, var left: TreeNode?, var right: TreeNode?) {
 
     constructor(value: Int) : this(value, null, null)
+}
+
+/**
+ * 将数组构建成二叉树
+ */
+fun arrayToTree(arr: Array<out Int?>): TreeNode? {
+    if (arr.isEmpty() || arr[0] == null) {
+        return null
+    }
+    val root = arr[0]?.let { TreeNode(it) }
+    val tree = mutableListOf<TreeNode?>()
+    tree.add(root)
+    for (i in 0..arr.size / 2 - 1) {
+        val left = arr[2 * i + 1]?.let { TreeNode(it) }
+        val right = arr[2 * i + 2]?.let { TreeNode(it) }
+        tree[i]?.left = left
+        tree[i]?.right = right
+        tree.add(left)
+        tree.add(right)
+    }
+    return root
+}
+
+/**
+ * 将二叉树转换成数组
+ */
+fun treeToArray(node: TreeNode?): Array<Int?> {
+    if (node == null) {
+        return arrayOf()
+    }
+    val list = mutableListOf<Int?>()
+    val queue = LinkedList<TreeNode?>()
+    queue.offer(node)
+    while (!queue.isEmpty()) {
+        var size = queue.size
+        while (size > 0) {
+            val tmp = queue.poll()
+            list.add(tmp?.value)
+            if (tmp != null) {
+                queue.offer(tmp.left)
+                queue.offer(tmp.right)
+            }
+            size--
+        }
+    }
+    for (i in list.size - 1 downTo 0) {
+        if (list[i] == null) {
+            list.removeAt(i)
+        } else{
+            break
+        }
+    }
+    return list.toTypedArray()
 }
